aaa570c59ee32f7ecde9f1b4e3ac5a241a5dbe4c
Alexander Pope <alex@pope-industries.com>
2015-05-06 10:58:19 +0200

    update query-string dependency

--- test/lib.js
+++ test/lib.js
@@ -763,73 +763,55 @@ require.register('lib/response.js', function(module, exports, require) {
     return this;
   };
 });
-require.register('query-string#1.0.0', function(module, exports, require) {
-  /*!
-  	query-string
-  	Parse and stringify URL query strings
-  	https://github.com/sindresorhus/query-string
-  	by Sindre Sorhus
-  	MIT License
-  */
-  (function () {
-  	'use strict';
-  	var queryString = {};
+require.register('query-string#2.0.0', function(module, exports, require) {
+  'use strict';
   
-  	queryString.parse = function (str) {
-  		if (typeof str !== 'string') {
-  			return {};
-  		}
-  
-  		str = str.trim().replace(/^(\?|#)/, '');
-  
-  		if (!str) {
-  			return {};
-  		}
-  
-  		return str.trim().split('&').reduce(function (ret, param) {
-  			var parts = param.replace(/\+/g, ' ').split('=');
-  			var key = parts[0];
-  			var val = parts[1];
-  
-  			key = decodeURIComponent(key);
-  			// missing `=` should be `null`:
-  			// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
-  			val = val === undefined ? null : decodeURIComponent(val);
-  
-  			if (!ret.hasOwnProperty(key)) {
-  				ret[key] = val;
-  			} else if (Array.isArray(ret[key])) {
-  				ret[key].push(val);
-  			} else {
-  				ret[key] = [ret[key], val];
-  			}
-  
-  			return ret;
-  		}, {});
-  	};
-  
-  	queryString.stringify = function (obj) {
-  		return obj ? Object.keys(obj).map(function (key) {
-  			var val = obj[key];
-  
-  			if (Array.isArray(val)) {
-  				return val.map(function (val2) {
-  					return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
-  				}).join('&');
-  			}
-  
-  			return encodeURIComponent(key) + '=' + encodeURIComponent(val);
-  		}).join('&') : '';
-  	};
-  
-  	if (typeof define === 'function' && define.amd) {
-  		define(function() { return queryString; });
-  	} else if (typeof module !== 'undefined' && module.exports) {
-  		module.exports = queryString;
-  	} else {
-  		window.queryString = queryString;
+  exports.parse = function (str) {
+  	if (typeof str !== 'string') {
+  		return {};
   	}
-  })();
+  
+  	str = str.trim().replace(/^(\?|#)/, '');
+  
+  	if (!str) {
+  		return {};
+  	}
+  
+  	return str.trim().split('&').reduce(function (ret, param) {
+  		var parts = param.replace(/\+/g, ' ').split('=');
+  		var key = parts[0];
+  		var val = parts[1];
+  
+  		key = decodeURIComponent(key);
+  		// missing `=` should be `null`:
+  		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+  		val = val === undefined ? null : decodeURIComponent(val);
+  
+  		if (!ret.hasOwnProperty(key)) {
+  			ret[key] = val;
+  		} else if (Array.isArray(ret[key])) {
+  			ret[key].push(val);
+  		} else {
+  			ret[key] = [ret[key], val];
+  		}
+  
+  		return ret;
+  	}, {});
+  };
+  
+  exports.stringify = function (obj) {
+  	return obj ? Object.keys(obj).map(function (key) {
+  		var val = obj[key];
+  
+  		if (Array.isArray(val)) {
+  			return val.map(function (val2) {
+  				return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
+  			}).join('&');
+  		}
+  
+  		return encodeURIComponent(key) + '=' + encodeURIComponent(val);
+  	}).join('&') : '';
+  };
   
 });
 require.register('lib/safeMerge.js', function(module, exports, require) {
@@ -1185,7 +1167,7 @@ require.register('lib/request.js', function(module, exports, require) {
   var cookie = require('cookie#0.1.2')
   	, emitter = require('eventemitter3#1.0.1')
   	, merge = require('lib/safeMerge.js')
-  	, qsParse = require('query-string#1.0.0').parse
+  	, qsParse = require('query-string#2.0.0').parse
   	, urlUtils = require('@yr/url-utils#1.8.1');
   
   module.exports = Request;
