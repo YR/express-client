6cc493d80c1303e6e05a95266d63f093120b38a1
Alexander Pope <alex@pope-industries.com>
2016-01-12 08:48:05 +0100

    spaces

--- src/lib/response.js
+++ src/lib/response.js
@@ -5,91 +5,91 @@
  */
 
 const assign = require('object-assign')
-	, cookie = require('cookie')
-	, Emitter = require('eventemitter3');
+  , cookie = require('cookie')
+  , Emitter = require('eventemitter3');
 
 /**
  * Instance factory
  */
 module.exports = function () {
-	return new Response();
+  return new Response();
 };
 
 class Response extends Emitter {
-	/**
-	 * Constructor
-	 */
-	constructor () {
-		super();
+  /**
+   * Constructor
+   */
+  constructor () {
+    super();
 
-		this.app;
-		this.req;
-		this.reset();
-	}
+    this.app;
+    this.req;
+    this.reset();
+  }
 
-	/**
-	 * Reset state
-	 */
-	reset () {
-		this.cached = false;
-		this.finished = false;
-		this.locals = {};
-		this.statusCode = 404;
-	}
+  /**
+   * Reset state
+   */
+  reset () {
+    this.cached = false;
+    this.finished = false;
+    this.locals = {};
+    this.statusCode = 404;
+  }
 
-	/**
-	 * Set status 'code'
-	 * @param {Number} code
-	 * @returns {Response}
-	 */
-	status (code) {
-		this.statusCode = code;
-		return this;
-	}
+  /**
+   * Set status 'code'
+   * @param {Number} code
+   * @returns {Response}
+   */
+  status (code) {
+    this.statusCode = code;
+    return this;
+  }
 
-	/**
-	 * Send response (last method called in pipeline)
-	 */
-	send () {
-		// Reset state
-		this.req.reset();
-		this.status(200);
-		this.finished = true;
-		this.emit('finish');
-	}
+  /**
+   * Send response (last method called in pipeline)
+   */
+  send () {
+    // Reset state
+    this.req.reset();
+    this.status(200);
+    this.finished = true;
+    this.emit('finish');
+  }
 
-	/**
-	 * Redirect to 'url'
-	 * @param {Number} statusCode
-	 * @param {String} url
-	 */
-	redirect (statusCode, url) {
-		this.app.redirectTo(url || statusCode);
-	}
+  /**
+   * Redirect to 'url'
+   * @param {Number} statusCode
+   * @param {String} url
+   */
+  redirect (statusCode, url) {
+    this.app.redirectTo(url || statusCode);
+  }
 
-	/**
-	 * Set cookie
-	 * @param {String} name
-	 * @param {String|Object} val
-	 * @param {Object} options
-	 * @returns {Response}
-	 */
-	cookie (name, val, options) {
-	  options = assign({}, options);
+  /**
+   * Set cookie
+   * @param {String} name
+   * @param {String|Object} val
+   * @param {Object} options
+   * @returns {Response}
+   */
+  cookie (name, val, options) {
+    options = assign({}, options);
 
-	  if ('number' == typeof val) val = val.toString();
-	  if ('object' == typeof val) val = 'j:' + JSON.stringify(val);
+    if ('number' == typeof val) val = val.toString();
+    if ('object' == typeof val) val = 'j:' + JSON.stringify(val);
 
-	  if ('maxAge' in options) {
-	    options.expires = new Date(Date.now() + options.maxAge);
-	    options.maxAge /= 1000;
-	  }
+    if ('maxAge' in options) {
+      options.expires = new Date(Date.now() + options.maxAge);
+      options.maxAge /= 1000;
+    }
 
-	  if (null == options.path) options.path = '/';
-	  const headerVal = cookie.serialize(name, String(val), options);
+    if (null == options.path) options.path = '/';
+    const headerVal = cookie.serialize(name, String(val), options);
 
-	  document.cookie = headerVal;
+    document.cookie = headerVal;
 
-	  return this;
-	}
+    return this;
+  }
 }
--- src/lib/router.js
+++ src/lib/router.js
@@ -6,218 +6,218 @@
  */
 
 const assign = require('object-assign')
-	, debug = require('debug')('express:router')
-	, layer = require('./layer')
-	, urlUtils = require('@yr/url-utils')
+  , debug = require('debug')('express:router')
+  , layer = require('./layer')
+  , urlUtils = require('@yr/url-utils')
 
-	, DEFAULTS = {
-			mergeParams: true,
-			caseSensitive: false,
-			strict: false
-		};
+  , DEFAULTS = {
+      mergeParams: true,
+      caseSensitive: false,
+      strict: false
+    };
 
 /**
  * Instance factory
  * @param {Object} [options]
  */
 module.exports = function (options) {
-	return new Router(options);
+  return new Router(options);
 };
 
 class Router {
-	/**
-	 * Constructor
-	 * @param {Object} [options]
-	 */
-	constructor (options) {
-		options = assign({}, DEFAULTS, options);
+  /**
+   * Constructor
+   * @param {Object} [options]
+   */
+  constructor (options) {
+    options = assign({}, DEFAULTS, options);
 
-		let boundMethod = this.method.bind(this);
+    let boundMethod = this.method.bind(this);
 
-		this.all = boundMethod;
-		this.get = boundMethod;
-		this.post = boundMethod;
-		this.handle = this.handle.bind(this);
-		this.stack = [];
-		this.mergeParams = options.mergeParams;
-		// Init matcher options
-		this.matcherOpts = {
-			sensitive: options.caseSensitive,
-			strict: options.strict,
-			end: false
-		};
-		this.strictMatcherOpts = {
-			sensitive: options.caseSensitive,
-			strict: options.strict,
-			end: true
-		};
-		this.params;
-	}
+    this.all = boundMethod;
+    this.get = boundMethod;
+    this.post = boundMethod;
+    this.handle = this.handle.bind(this);
+    this.stack = [];
+    this.mergeParams = options.mergeParams;
+    // Init matcher options
+    this.matcherOpts = {
+      sensitive: options.caseSensitive,
+      strict: options.strict,
+      end: false
+    };
+    this.strictMatcherOpts = {
+      sensitive: options.caseSensitive,
+      strict: options.strict,
+      end: true
+    };
+    this.params;
+  }
 
-	/**
-	 * Handle param 'name' with 'fn'
-	 * @param {String} name
-	 * @param {Function} fn(req, res, next, value)
-	 */
-	param (name, fn) {
-		if (!this.params) this.params = {};
-		this.params[name] = fn;
-	}
+  /**
+   * Handle param 'name' with 'fn'
+   * @param {String} name
+   * @param {Function} fn(req, res, next, value)
+   */
+  param (name, fn) {
+    if (!this.params) this.params = {};
+    this.params[name] = fn;
+  }
 
-	/**
-	 * Add one or more 'fn' to middleware pipeline at optional 'path'
-	 * @param {Function} fn
-	 */
-	use (/* path, */ fn /* ...fn */) {
-		let offset = 0
-			, path = '/'
-			, fns;
+  /**
+   * Add one or more 'fn' to middleware pipeline at optional 'path'
+   * @param {Function} fn
+   */
+  use (/* path, */ fn /* ...fn */) {
+    let offset = 0
+      , path = '/'
+      , fns;
 
-		if ('string' == typeof fn) {
-			offset = 1;
-			path = fn;
-		}
+    if ('string' == typeof fn) {
+      offset = 1;
+      path = fn;
+    }
 
-		fns = Array.prototype.slice.call(arguments, offset);
+    fns = Array.prototype.slice.call(arguments, offset);
 
-		fns.forEach(function (fn) {
-			if (fn instanceof Router) {
-				fn = fn.handle;
-			}
-			const lyr = layer(path, fn, this.matcherOpts);
+    fns.forEach(function (fn) {
+      if (fn instanceof Router) {
+        fn = fn.handle;
+      }
+      const lyr = layer(path, fn, this.matcherOpts);
 
-			debug('adding router middleware %s with path %s', lyr.name, path);
-			this.stack.push(lyr);
-		}, this);
-	}
+      debug('adding router middleware %s with path %s', lyr.name, path);
+      this.stack.push(lyr);
+    }, this);
+  }
 
-	/**
-	 * Register method at 'path'
-	 * @param {String} path
-	 */
-	method (path) {
-		const fns = Array.prototype.slice.call(arguments, 1);
+  /**
+   * Register method at 'path'
+   * @param {String} path
+   */
+  method (path) {
+    const fns = Array.prototype.slice.call(arguments, 1);
 
-		fns.forEach(function (fn) {
-			let lyr = layer(path, fn, this.strictMatcherOpts);
+    fns.forEach(function (fn) {
+      let lyr = layer(path, fn, this.strictMatcherOpts);
 
-			lyr.route = true;
+      lyr.route = true;
 
-			debug('adding router route %s with path %s', lyr.name, path);
-			this.stack.push(lyr);
-		}, this);
-	}
+      debug('adding router route %s with path %s', lyr.name, path);
+      this.stack.push(lyr);
+    }, this);
+  }
 
-	/**
-	 * Run request/response through middleware pipline
-	 * @param {Request} req
-	 * @param {Response} res
-	 * @param {Function} done
-	 */
-	handle (req, res, done) {
-		const self = this
-			, parentUrl = req.baseUrl || '';
+  /**
+   * Run request/response through middleware pipline
+   * @param {Request} req
+   * @param {Response} res
+   * @param {Function} done
+   */
+  handle (req, res, done) {
+    const self = this
+      , parentUrl = req.baseUrl || '';
 
-		let idx = 0
-			, processedParams = {}
-			, removed = '';
+    let idx = 0
+      , processedParams = {}
+      , removed = '';
 
-		// Update done to restore req props
-		done = restore(done, req, 'baseUrl', 'next', 'params');
+    // Update done to restore req props
+    done = restore(done, req, 'baseUrl', 'next', 'params');
 
-		// Setup next layer
-		req.next = next;
-		req.baseUrl = parentUrl;
+    // Setup next layer
+    req.next = next;
+    req.baseUrl = parentUrl;
 
-		next();
+    next();
 
-		function next (err) {
-			var lyr = self.stack[idx++]
-				, layerErr = err;
+    function next (err) {
+      var lyr = self.stack[idx++]
+        , layerErr = err;
 
-			if (removed.length != 0) {
-				debug('untrim %s from url %s', removed, req.path);
-				req.baseUrl = parentUrl;
-				req.path = urlUtils.join(removed, req.path);
-				removed = '';
-			}
+      if (removed.length != 0) {
+        debug('untrim %s from url %s', removed, req.path);
+        req.baseUrl = parentUrl;
+        req.path = urlUtils.join(removed, req.path);
+        removed = '';
+      }
 
-			// Exit
-			if (!lyr) return done(err);
+      // Exit
+      if (!lyr) return done(err);
 
-			// Skip if no match
-			if (!lyr.match(req.path)) return next(err);
+      // Skip if no match
+      if (!lyr.match(req.path)) return next(err);
 
-			debug('%s matched layer %s with path %s', req.path, lyr.name, lyr.path);
+      debug('%s matched layer %s with path %s', req.path, lyr.name, lyr.path);
 
-			// Store params
-			if (self.mergeParams) {
-				if (!req.params) req.params = {};
-				assign(req.params, lyr.params);
-			} else {
-				req.params = lyr.params;
-			}
+      // Store params
+      if (self.mergeParams) {
+        if (!req.params) req.params = {};
+        assign(req.params, lyr.params);
+      } else {
+        req.params = lyr.params;
+      }
 
-			const keys = Object.keys(lyr.params);
+      const keys = Object.keys(lyr.params);
 
-			// Process params if necessary
-			self._processParams(processedParams, req.params, keys, req, res, function (err) {
-				if (err) return next(layerErr || err);
-				if (!lyr.route) trim(lyr);
-				return lyr.handle(layerErr, req, res, next);
-			});
-		}
+      // Process params if necessary
+      self._processParams(processedParams, req.params, keys, req, res, function (err) {
+        if (err) return next(layerErr || err);
+        if (!lyr.route) trim(lyr);
+        return lyr.handle(layerErr, req, res, next);
+      });
+    }
 
-		function trim (layer) {
-			if (layer.path.length != 0) {
-				debug('trim %s from url %s', layer.path, req.path);
-				removed = layer.path;
-				req.path = req.path.substr(removed.length);
-				if (req.path.charAt(0) != '/') req.path = '/' + req.path;
+    function trim (layer) {
+      if (layer.path.length != 0) {
+        debug('trim %s from url %s', layer.path, req.path);
+        removed = layer.path;
+        req.path = req.path.substr(removed.length);
+        if (req.path.charAt(0) != '/') req.path = '/' + req.path;
 
-				req.baseUrl = urlUtils.join(parentUrl, removed);
-			}
-		}
-	}
+        req.baseUrl = urlUtils.join(parentUrl, removed);
+      }
+    }
+  }
 
-	/**
-	 * Process middleware matched parameters
-	 * @param {Object} processedParams
-	 * @param {Object} params
-	 * @param {Array} keys
-	 * @param {Request} req
-	 * @param {Response} res
-	 * @param {Function} done(err)
-	 */
-	_processParams (processedParams, params, keys, req, res, done) {
-		const self = this;
+  /**
+   * Process middleware matched parameters
+   * @param {Object} processedParams
+   * @param {Object} params
+   * @param {Array} keys
+   * @param {Request} req
+   * @param {Response} res
+   * @param {Function} done(err)
+   */
+  _processParams (processedParams, params, keys, req, res, done) {
+    const self = this;
 
-		let idx = 0;
+    let idx = 0;
 
-		function next (err) {
-			// Stop processing on any error
-			if (err) return done(err);
+    function next (err) {
+      // Stop processing on any error
+      if (err) return done(err);
 
-			if (idx >= keys.length) return done();
+      if (idx >= keys.length) return done();
 
-			const name = keys[idx++]
-				, fn = self.params[name];
+      const name = keys[idx++]
+        , fn = self.params[name];
 
-			// Process if match and not already processed
-			if (fn && !processedParams[name]) {
-				processedParams[name] = true;
-				fn(req, res, next, params[name]);
-			} else {
-				next();
-			}
-		}
+      // Process if match and not already processed
+      if (fn && !processedParams[name]) {
+        processedParams[name] = true;
+        fn(req, res, next, params[name]);
+      } else {
+        next();
+      }
+    }
 
-		if (this.params && keys.length) {
-			next();
-		} else {
-			done();
-		}
-	}
+    if (this.params && keys.length) {
+      next();
+    } else {
+      done();
+    }
+  }
 }
 
 /**
@@ -226,20 +226,20 @@ class Router {
  * @param {Object} obj
  */
 function restore (fn, obj) {
-	let props = new Array(arguments.length - 2)
-		, vals = new Array(arguments.length - 2);
+  let props = new Array(arguments.length - 2)
+    , vals = new Array(arguments.length - 2);
 
-	for (let i = 0; i < props.length; i++) {
-		props[i] = arguments[i + 2];
-		vals[i] = obj[props[i]];
-	}
+  for (let i = 0; i < props.length; i++) {
+    props[i] = arguments[i + 2];
+    vals[i] = obj[props[i]];
+  }
 
-	return function () {
-		// Restore vals
-		for (let i = 0; i < props.length; i++) {
-			obj[props[i]] = vals[i];
-		}
+  return function () {
+    // Restore vals
+    for (let i = 0; i < props.length; i++) {
+      obj[props[i]] = vals[i];
+    }
 
-		return fn.apply(this, arguments);
-	};
+    return fn.apply(this, arguments);
+  };
 }
