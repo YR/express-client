43bbeb9a66167ea595d954e306b6248a5ba9695c
Alexander Pope <alex@pope-industries.com>
2016-02-22 13:58:38 +0100

    update dependencies; rebuild

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * An express.js framework for the browser
+ * https://github.com/yr/express-client
+ * @copyright Yr
+ * @license MIT
+ */
+
 const application = require('./lib/application')
   , Router = require('./lib/router');
 
